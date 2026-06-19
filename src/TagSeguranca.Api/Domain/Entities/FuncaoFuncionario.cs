ebb7449d6fdb9fe1bd74b013cc7edbaa477f1816
Antonio Neto <antonioolimpiodesaneto@gmail.com>
2026-06-19 18:30:29 -0300

    feat: add employee role entity

---
+++ src/TagSeguranca.Api/Domain/Entities/FuncaoFuncionario.cs
@@ -0,0 +1,12 @@
+namespace TagSeguranca.Api.Domain.Entities;
+
+public class FuncaoFuncionario
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Nome { get; set; } = string.Empty;
+    public bool Ativo { get; set; } = true;
+    public Guid? UsuarioCriacaoId { get; set; }
+    public DateTime DataCriacao { get; set; } = DateTime.UtcNow;
+    public Guid? UsuarioAlteracaoId { get; set; }
+    public DateTime? DataAlteracao { get; set; }
+}
