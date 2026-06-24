ba77c41f509ef83f8dfd2d57257124de5deb62b4
Antonio Neto <antonioolimpiodesaneto@gmail.com>
2026-06-24 01:59:21 -0300

    feat: ajustar detalhe financeiro

--- src/TagSeguranca.Api/Application/Pagamentos/PagamentoPendenteDetalheResponse.cs
+++ src/TagSeguranca.Api/Application/Pagamentos/PagamentoPendenteDetalheResponse.cs
@@ -7,6 +7,7 @@ public class PagamentoPendenteDetalheResponse
     public string NomeCompleto { get; set; } = string.Empty;
     public string Rg { get; set; } = string.Empty;
     public string Cpf { get; set; } = string.Empty;
+    public string Funcao { get; set; } = string.Empty;
     public string MeioPagamento { get; set; } = string.Empty;
 
     public int QuantidadeEventos { get; set; }
